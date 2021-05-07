f6333a0a2afbc93c33c59ab38fea7838d7994f8e
shun2015 <dwat.pmjg@gmail.com>
2021-05-07 17:40:12 +0900

    update a

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts, only: [:new, :create, :index, :show, :destroy]
   resources :users, only: [:index, :show, :edit, :update]
+  namespace :admin do
+    resources :users, only:[:index]
+  end
 end
